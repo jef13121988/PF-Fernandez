d35e6a970f73904ccc79168edcf636bb904414b3
jef13121988 <jef_1988@hotmail.com>
2024-05-29 22:39:58 -0300

    Corregidas cards y mejorado footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,7 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className='footer__style'>
+            <a href="https://jef13121988.github.io/ProyectoFinal-JavaScript/" className='link__style'>Inspirado por el Juegazo</a>
             <p> By Jonás | 2024 </p>
         </footer>
     )
